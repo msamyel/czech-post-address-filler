76370e40562d6b81d844307eedcbc5cb02074dc3
Voslar Matej <msamyel@gmail.com>
2024-10-15 18:13:42 +0200

    update build script to create platform specific dist packages

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 rm -rf dist/
 mkdir dist
+mkdir dist/intermediate
+mkdir dist/intermediate/chrome
+mkdir dist/intermediate/firefox
+cp -r src/* dist/intermediate/chrome
+cp -r src/* dist/intermediate/firefox
+cp -r platform_specific/chrome/* dist/intermediate/chrome
+cp -r platform_specific/firefox/* dist/intermediate/firefox
+mkdir dist/firefox
 mkdir dist/chrome
-cd ./src
-zip -r ../dist/chrome/chrome.zip *
+cd dist/intermediate/firefox
+zip -r ../../firefox/firefox.zip *
+cd ../chrome
+zip -r ../../chrome/chrome.zip *
